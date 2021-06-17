ee76751c3706a6111078714b22744b286319fbeb
猎艳 <863448246@qq.com>
2021-06-17 15:08:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("java", "-javaagent:BurpLoaderKeygen.jar", "-noverify", "-jar", "burpsuite_pro_v2021.5.3.jar")
+	cmd := exec.Command("java", "-javaagent:BurpLoaderKeygen.jar", "-noverify", "-jar", "burpsuite_pro.jar")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	cmd.Start()
 }
